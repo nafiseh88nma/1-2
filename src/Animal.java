5c3a03374797992933256aecb055357a35764145
nafiseh <fun101.nm@gmail.com>
2020-01-29 13:19:39 +0330

    homework 1-2

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+/**
+ * Created by admin on 9/23/2019.
+ */
+public class Animal {
+    public void eat(){
+        System.out.println("cat is eating");
+
+    }
+    public void sleep(){
+        System.out.println("cat is sleeping");
+    }
+    public void run(){
+        System.out.println("cat is running");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 9/23/2019.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Animal cat= new Animal();
+        cat.sleep();
+
+    }
+}
